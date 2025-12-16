72cc3be58627b209af9d61db9f451dd0e05bfdf3
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-16 11:11:16 +0330

    delete user api added

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -12,3 +12,8 @@ export const createUser = async (user: CreateUserDto): Promise<User> => {
     const res = await axiosClient.post<User>("/users" , user);
     return res.data
 };
+
+export const deleteUser = async (id:string):Promise<void> => {
+    const res = await axiosClient.delete("/users/${id}")
+    return res.data
+}
