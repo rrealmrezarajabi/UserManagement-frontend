5d13028231ed13041d1cdcc472526133a3c5c333
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-16 10:20:21 +0330

    create user api added on userapi on api folder

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,9 +1,14 @@
 import { axiosClient } from "./axiosClient";
 
-import type {User} from "../types/user"
+import type {User , CreateUserDto} from "../types/user"
 
 
 export const getUsers = async ():Promise<User[]> => {
     const res = await axiosClient.get<User[]>("/users")
     return res.data
 }
+
+export const createUser = async (user: CreateUserDto): Promise<User> => {
+    const res = await axiosClient.post<User>("/users" , user);
+    return res.data
+};
